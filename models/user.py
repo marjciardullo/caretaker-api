817f3361cf131d8e1486c08694be987a3716e970
Julia <juliagatoh@gmail.com>
2023-04-27 20:33:24 -0300

    implementado cors, fallback secret key, mudanças em views e models

--- models/user.py
+++ models/user.py
@@ -5,12 +5,14 @@ class User(db.Model):
     __tablename__ = "usuario"
 
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
     nome = db.Column(db.String(100), unique=False)
     email = db.Column(db.String(100))
     senha = db.Column(db.String(10))
     nascimento = db.Column(db.String(100))
 
-    def __init__(self, nome, email, senha, nascimento) -> None:
+    def __init__(self, username, nome, email, senha, nascimento) -> None:
+        self.username = username
         self.nome = nome
         self.email = email
         self.senha = senha
@@ -31,3 +33,11 @@ class User(db.Model):
     @classmethod
     def find_user_by_email(cls, email):
         return cls.query.filter_by(email=email).first()
+
+    @classmethod
+    def find_user_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+
+    @classmethod
+    def find_user_by_login(cls, username, password):
+        return cls.query.filter_by(username=username, senha=password).first()
